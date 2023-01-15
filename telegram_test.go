6f32933a6b4d40d293f1c6891df8ae187ac7a4ba
smallyunet <dev.wangyu@gmail.com>
2023-01-15 14:19:16 +0800

    fix test

--- telegram_test.go
+++ telegram_test.go
@@ -21,7 +21,7 @@ func Test_sendMsgText(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			sendMsgText(tt.args.text)
+			SendMsgText(tt.args.text)
 		})
 	}
 }
